41bbb5cc93d8e2d41d7e3de6846423b96a2d4f83
Billal Asif <asif.billal@outlook.com>
2021-12-27 23:05:40 +0000

    general update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 
 var canvas = document.getElementById("mainCanvas");
 var c = canvas.getContext("2d");
+/* window.addEventListener('load', resize())
+window.addEventListener('resize', () => {
+    resize();
+    drawPlayer();
+}, true); */
+
 
 class Player {
 
@@ -14,7 +20,6 @@ class Player {
     }
 
     draw() {
-
 
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
@@ -39,15 +44,41 @@ class Player {
 }
 
 
-let randomEnemies = () => {
+//Functions
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function randomEnemies() {
+
+    let randomNum = () => {
+        const arr = [16,17,18,19,20,21,22,23,24,25];
+        const random = Math.floor(Math.random() * 10);
+        const randomArr = arr[random];
+        return randomArr;
+    }
+    const randomX = window.innerHeight / Math.floor(Math.random() * 10);
+    const randomY = window.innerWidth / Math.floor(Math.random() * 10);
+    const randomSize = randomNum();
+    const randomSixDigit = Math.floor(Math.random() * 16777215).toString(16);
+    const randomColour = "#" + randomSixDigit;
 
     //creates circles randomly
+    const enemy = new Player(randomX, randomY, randomSize, randomColour)
+    enemy.draw();
 
 }
 
+function drawPlayer() {
 
-let x = canvas.width / 2;
-let y = canvas.height / 2;
-//Player start position
-const player = new Player(x, y, 20, "red");
-player.draw();
+    let x = canvas.width / 2;
+    let y = canvas.height / 2;
+    //Player start position
+    const player = new Player(x, y, 20, "red");
+    player.draw();
+
+}
+
+//Start game
+drawPlayer();
